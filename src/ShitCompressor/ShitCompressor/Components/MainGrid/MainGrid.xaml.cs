0e6a4f3bffc909549d818bfb4b6b684f5f08fc83
Jia Liang <hojialiang@yahoo.com>
2020-12-24 18:55:57 +0800

    Add loading message when adding images

--- src/ShitCompressor/ShitCompressor/Components/MainGrid/MainGrid.xaml.cs
+++ src/ShitCompressor/ShitCompressor/Components/MainGrid/MainGrid.xaml.cs
@@ -5,6 +5,7 @@
     using System.Collections.Generic;
     using System.IO;
     using System.Linq;
+    using System.Threading.Tasks;
     using System.Windows;
     using System.Windows.Controls;
     using System.Windows.Input;
@@ -41,7 +42,7 @@
             e.Handled = true;
         }
 
-        private void File_Drop(object sender, DragEventArgs e) {
+        private async void File_Drop(object sender, DragEventArgs e) {
             List<string> errorMessages = new List<string>();
             string[] droppedFilePathnames = (string[]) e.Data.GetData(DataFormats.FileDrop);
 
@@ -51,47 +52,56 @@
                 errorMessages.Add(errorParsingSettings);
             }
 
-            foreach (string pathname in droppedFilePathnames) {
-                FileInfo inputInfo = new FileInfo(pathname);
+            LoadingCover.Visibility = Visibility.Visible;
 
-                if (!Globals.ValidExtensions.Contains(inputInfo.Extension.ToLower())) {
-                    errorMessages.Add($"{pathname} has an invalid extension.");
-                    return;
+            await Task.Run(() => {
+                foreach (string pathname in droppedFilePathnames) {
+                    FileInfo inputInfo = new FileInfo(pathname);
+
+                    if (!Globals.ValidExtensions.Contains(inputInfo.Extension.ToLower())) {
+                        errorMessages.Add($"{pathname} has an invalid extension.");
+                        continue;
+                    }
+
+                    if (ImageList.ToList().SingleOrDefault(image => image.InputInfo.FullName == pathname) != null) {
+                        errorMessages.Add($"{pathname} is a duplicate");
+                        continue;
+                    }
+
+                    BitmapImage preview;
+
+                    try {
+                        preview = new BitmapImage();
+                        preview.BeginInit();
+                        preview.CacheOption = BitmapCacheOption.OnLoad;
+                        preview.UriSource = new Uri(pathname);
+                        preview.EndInit();
+                        preview.Freeze();
+                    } catch (Exception exception) {
+                        errorMessages.Add($"Error decoding {pathname}: {exception.Message}");
+                        return;
+                    };
+
+                    try {
+                        using FileStream fs = new FileStream(pathname, FileMode.Open, FileAccess.ReadWrite, FileShare.Read);
+                    } catch (Exception exception) {
+                        errorMessages.Add($"Permission error with {pathname}: {exception.Message}");
+                        return;
+                    }
+
+                    Application.Current.Dispatcher.Invoke(
+                        new Action(() => { ImageList.Add(new CImage(inputInfo, preview)); })
+                    );
+
+                    MainWindow.PathsToCleanupOnClose.Add(Path.Combine(Path.GetDirectoryName(pathname), Globals.TempStorageFolder));
                 }
-
-                if (ImageList.SingleOrDefault(image => image.InputInfo.FullName == pathname) != null) {
-                    errorMessages.Add($"{pathname} is a duplicate");
-                    return;
-                }
-
-                BitmapImage preview;
-
-                try {
-                    preview = new BitmapImage();
-                    preview.BeginInit();
-                    preview.CacheOption = BitmapCacheOption.OnLoad;
-                    preview.UriSource = new Uri(pathname);
-                    preview.EndInit();
-                    preview.Freeze();
-                } catch (Exception exception) {
-                    errorMessages.Add($"Error decoding {pathname}: {exception.Message}");
-                    continue;
-                };
-
-                try {
-                    using FileStream fs = new FileStream(pathname, FileMode.Open, FileAccess.ReadWrite, FileShare.Read);
-                } catch (Exception exception) {
-                    errorMessages.Add($"Permission error with {pathname}: {exception.Message}");
-                    continue;
-                }
-
-                ImageList.Add(new CImage(inputInfo, preview));
-                MainWindow.PathsToCleanupOnClose.Add(Path.Combine(Path.GetDirectoryName(pathname), Globals.TempStorageFolder));
-            }
+            });
 
             if (errorMessages.Count != 0) {
                 Utilities.Alert(string.Join("\n\n", errorMessages.ToArray()));
             }
+
+            LoadingCover.Visibility = Visibility.Collapsed;
         }
 
         private void CheckBox_Checked(object sender, RoutedEventArgs e) {
