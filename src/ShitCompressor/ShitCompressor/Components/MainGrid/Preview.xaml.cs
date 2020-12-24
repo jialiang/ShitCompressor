9f2f7f5995e0e167a772e0bad33262abc4275ec7
Jia Liang <hojialiang@yahoo.com>
2020-12-25 02:12:51 +0800

    Allow click on preview image to open image in explorer

--- src/ShitCompressor/ShitCompressor/Components/MainGrid/Preview.xaml.cs
+++ src/ShitCompressor/ShitCompressor/Components/MainGrid/Preview.xaml.cs
@@ -1,9 +1,17 @@
 ﻿namespace ShitCompressor.Components.MainGrid {
+    using ShitCompressor.Classes;
+    using System.Windows;
     using System.Windows.Controls;
+    using System.Windows.Input;
 
     public partial class Preview : UserControl {
         public Preview() {
             InitializeComponent();
+        }
+
+        private void Image_MouseLeftButtonUp(object sender, MouseButtonEventArgs e) {
+            CImage image = (CImage) (sender as FrameworkElement).DataContext;
+            image.OpenInput();
         }
     }
 }
